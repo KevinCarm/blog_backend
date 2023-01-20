7f95fcf39ffd441bafff685bd184cd53f5abcd7c
Kevin Carmona Serrano <kevincarm016@hotmail.com>
2023-01-19 19:49:57 -0600

    Bad functionality in get by email method in user repository fixed

--- src/main/java/com/kev/app/blog/entities/repositories/IUserRepository.java
+++ src/main/java/com/kev/app/blog/entities/repositories/IUserRepository.java
@@ -3,11 +3,10 @@ package com.kev.app.blog.entities.repositories;
 import com.kev.app.blog.entities.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface IUserRepository extends JpaRepository<User, Long> {
-    @Query(value = "SELECT u FROM User WHERE u.email = :email")
-    User getByEmail(@Param("email") String email);
+    @Query("SELECT u FROM User u WHERE u.email=?1")
+    User getByEmail(String email);
 }
