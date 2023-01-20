5f2dfaf44ca5b728cafe508b4ab924d8b7f09fb0
Kevin Carmona Serrano <kevincarm016@hotmail.com>
2023-01-19 20:14:59 -0600

    Permit any request to path /users with http method POST

--- src/main/java/com/kev/app/blog/configuration/AppConfiguration.java
+++ src/main/java/com/kev/app/blog/configuration/AppConfiguration.java
@@ -2,12 +2,29 @@ package com.kev.app.blog.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
-public class AppConfiguration {
+@EnableWebSecurity
+public class AppConfiguration extends WebSecurityConfigurerAdapter {
+
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf()
+                .disable()
+                .authorizeRequests()
+                .antMatchers(HttpMethod.POST, "/users")
+                .permitAll();
     }
 }
