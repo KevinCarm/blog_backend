585c4f3c73276e8ff77b1dd34bdd9c81f9436f2e
Kevin Carmona Serrano <kevincarm016@hotmail.com>
2023-01-20 12:58:50 -0600

    Save post with user relation

--- src/main/java/com/kev/app/blog/controllers/PostController.java
+++ src/main/java/com/kev/app/blog/controllers/PostController.java
@@ -1,10 +1,15 @@
 package com.kev.app.blog.controllers;
 
 import com.kev.app.blog.entities.models.Post;
+import com.kev.app.blog.entities.models.User;
 import com.kev.app.blog.entities.services.IPostService;
+import com.kev.app.blog.entities.services.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
@@ -15,9 +20,17 @@ import java.util.Optional;
 public class PostController {
     @Autowired
     private IPostService service;
+    @Autowired
+    private IUserService userService;
 
-    @PostMapping("/post")
+    @PostMapping(value = "/post", produces = {MediaType.APPLICATION_JSON_VALUE}, consumes = {MediaType.APPLICATION_JSON_VALUE})
     public ResponseEntity<?> save(@RequestBody Post post) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String currentPrincipalName = authentication.getName();
+        User user = userService.getByEmail(currentPrincipalName);
+        user.getPosts().add(post);
+        userService.save(user);
+        post.setUser(user);
         return ResponseEntity.status(HttpStatus.CREATED).body(service.save(post));
     }
 
